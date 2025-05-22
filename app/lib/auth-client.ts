f09d081d60f4532d47681759674989edee705310
willian-lemann <willianleman@hotmail.com>
2025-05-22 20:13:04 -0300

    feat: init

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -1,4 +1,5 @@
 import { createAuthClient } from "better-auth/react";
+console.log("BETTER_AUTH_URL", process.env.BETTER_AUTH_URL);
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
   baseURL: process.env.BETTER_AUTH_URL,
