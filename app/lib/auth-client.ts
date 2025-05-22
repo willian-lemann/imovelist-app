c64ab753f9ca4eed6c0445316bf398b00df8308a
willian-lemann <willianleman@hotmail.com>
2025-05-22 20:18:42 -0300

    feat: init

--- app/lib/auth-client.ts
+++ app/lib/auth-client.ts
@@ -3,5 +3,5 @@ import { createAuthClient } from "better-auth/react";
 console.log(import.meta.env.VITE_BETTER_AUTH_URL);
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: import.meta.env.BETTER_AUTH_URL,
+  baseURL: import.meta.env.VITE_BETTER_AUTH_URL,
 });
