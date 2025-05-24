976c47a2598d10f04705a1f11a85aa963751462d
willianleman <willian@spocket.co>
2025-05-24 16:15:40 -0300

    feat: ssl for database

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -4,6 +4,7 @@ import { Pool } from "pg";
 export const auth = betterAuth({
   database: new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: false,
   }),
   emailAndPassword: {
     enabled: true,
